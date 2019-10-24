9aa0c7213b788fded8aa69b4516638ce31244210
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-10-24 11:02:54 -0400

    these are just experiments made while investigating best code platform/language for phase2

---
+++ example.php
@@ -0,0 +1,93 @@
+<?php
+
+	define("GIT_REPO_PATH", "Users/mp/gitshow1"); //set the path to your repo here
+
+	$repo_dir = getenv('GIT_DIR');
+	if (empty($repo_dir)) {
+		chdir(GIT_REPO_PATH);
+		$repo_dir = GIT_REPO_PATH;
+	}
+	$repo_name = basename($repo_dir);
+
+	$cmd = 'git log --graph --date-order --all -C -M -n 100 --date=iso --pretty=format:"B[%d] C[%H] D[%ad] A[%an] E[%ae] H[%h] S[%s]"';
+
+	@ob_clean();
+	ob_start();
+	passthru($cmd . ' 2>&1');
+	$o = ob_get_clean();
+
+	$rawRows = explode("\n", $o);
+	$graphItems = array();
+
+	foreach ($rawRows as $row) {
+		if (preg_match("/^(.+?)(\s(B\[(.*?)\])? C\[(.+?)\] D\[(.+?)\] A\[(.+?)\] E\[(.+?)\] H\[(.+?)\] S\[(.+?)\])?$/", $row, $output)) {
+			if (!isset($output[4])) {
+				$graphItems[] = array(
+					"relation"=>$output[1]
+				);
+				continue;
+			}
+			$graphItems[] = array(
+				"relation"=>$output[1],
+				"branch"=>$output[4],
+				"rev"=>$output[5],
+				"date"=>$output[6],
+				// "author"=>$output[7],
+				// "author_email"=>$output[8],
+				"short_rev"=>$output[9],
+				"subject"=>preg_replace('/(^|\s)(#[[:xdigit:]]+)(\s|$)/', '$1<a href="$2">$2</a>$3', $output[10])
+			);
+		}
+	}
+
+	$title = "Git Graph of " . $repo_name;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $title; ?></title>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<script type="text/javascript" src="jquery.js"></script>
+		<script type="text/javascript" src="gitgraph.js"></script>
+		<script type="text/javascript" src="draw.js"></script>
+		<link href="gitgraph.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div id="header">
+			<h2>
+				<?php echo $title; ?>
+			</h2>
+		</div>
+		<div id="git-graph-container">
+		<div id="rel-container">
+			<canvas id="graph-canvas" width="100px">
+				<ul id="graph-raw-list">
+					<?php
+						foreach ($graphItems as $graphItem) {
+							echo "<li><span class=\"node-relation\">" . $graphItem['relation'] . "</span></li>\n";
+						}
+					?>
+				</ul>
+			</canvas>
+		</div>
+
+		<div style="float:left;" id="rev-container">
+			<ul id="rev-list">
+				<?php
+					foreach ($graphItems as $graphItem) {
+						echo "<li>";
+						if (isset($graphItem['rev'])) {
+							echo "<code id='".$graphItem['short_rev']."'>".$graphItem['short_rev']."</code> <strong>" . $graphItem['branch'] . "</strong> <em>" . $graphItem['subject'] . "</em> by <span class=\"author\">" . $graphItem['author'] . " &lt;" . $graphItem['author_email'] . "&gt;</span>  <span class=\"time\">" . $graphItem['date'] . "</span>";
+						} else {
+							echo "<span />";
+						}
+						echo "</li>";
+					}
+				?>
+			</ul>
+		</div>
+	</div>
+	</body>
+</html>
---
+++ vco.js
@@ -0,0 +1,41 @@
+// Voltage-controlled oscillator example
+// by Andrew Belt
+
+// JavaScript isn't ideal for audio generating and processing due to it being 10-100 less efficient than C++, but it's still an easy way to learn simple DSP.
+
+config.frameDivider = 1
+config.bufferSize = 16
+
+var phase = 0
+var phase2 = 0
+function process(block) {
+	// Knob ranges from -5 to 5 octaves
+	var pitch = block.knobs[0] * 10 - 5
+	// Input follows 1V/oct standard
+	pitch += block.inputs[0][0]		
+
+
+
+
+	// The relationship between 1V/oct pitch and frequency is `freq = 2^pitch`.
+	// Default frequency is middle C (C4) in Hz.
+	// https://vcvrack.com/manual/VoltageStandards.html#pitch-and-frequencies
+	var freq = 261.6256 * Math.pow(2, pitch)
+
+	display("Freq: " + freq.toFixed(3) + " Hz")
+
+	// Set all samples in output buffer
+	var deltaPhase = config.frameDivider * block.sampleTime * freq
+	for (var i = 0; i < block.bufferSize; i++) {
+		// Accumulate phase
+		phase += deltaPhase
+		// Wrap phase around range [0, 1]
+		phase %= 1
+		// Convert phase to sine output
+		block.outputs[0][i] = Math.sin(2 * Math.PI * phase) * 5
+		block.outputs[1][i] = Math.sin(2 * Math.PI * phase) * 5
+	}
+
+
+
+}
