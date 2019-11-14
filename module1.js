a4c1e970ef67d71094315444e6b2aa835dd3b69c
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-11-14 09:57:10 -0500

    wave shaper can now control the buffer size

--- module1.js
+++ module1.js
@@ -4,14 +4,14 @@
 // JavaScript isn't ideal for audio generating and processing due to it being 10-100 less efficient than C++, but it's still an easy way to learn simple DSP.
 
 config.frameDivider = 1
-config.bufferSize = 64
+config.bufferSize = 1
 
 let tape = []
 
 var phase = 0
 var phase2 = 0
 function process(block) {
-	tape.length = 44100
+	// tape.length = 44100
 	// Knob ranges from -5 to 5 octaves
 	var pitch = block.knobs[0] * 10 - 5
 	// Input follows 1V/oct standard
@@ -23,8 +23,8 @@ function process(block) {
 	// https://vcvrack.com/manual/VoltageStandards.html#pitch-and-frequencies
 	var freq = 111.6256 * Math.pow(2, pitch)
 
-	tap1 = Math.round(block.knobs[1] * 64)
-	
+	// crush = 
+	block.bufferSize += Math.round(block.knobs[1] * 1024)
 	
 	// Set all samples in output buffer
 	var deltaPhase = config.frameDivider * block.sampleTime * freq
@@ -35,9 +35,9 @@ function process(block) {
 		phase %= 1
 		// Convert phase to sine output
 		block.outputs[0][i] = Math.sin(2 * Math.PI * phase) * 5
-		tape[i] = (block.inputs[0][i])
-		block.outputs[1][i] = tape[tap1]
-		display(tape[i])
+		// tape[i] = (block.inputs[0][i])
+		// block.outputs[1][i] = tape[tap1]
+		display(block.bufferSize)
 
 	}
 
